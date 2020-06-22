de9d9606294757c1a75209e018ee746753920172
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-06-22 16:18:23 +0200

    Implement second plus third route

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,7 +4,31 @@ var pokedex = require('./pokedex.json')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json(pokedex);
+  res.send(pokedex);
+});
+
+router.get('/:id', function (req, res, next) {
+  var id = req.params;
+  var idAsNumber = parseInt(id, 10);
+
+  const foundPokemon = pokedex.find((singlePokemon) => {
+      return singlePokemon.id === idAsNumber;
+    });
+
+  res.send(id);
+
+});
+
+router.get('/:id/:info', function(req, res, next) {
+  var { id, info } = req.params;
+  var idAsNumber = parseInt(id, 10);
+
+  const foundPokemon = pokedex.find((singlePokemon) => {
+      return singlePokemon.id === idAsNumber;
+    });
+
+  res.send(foundPokemon[info]);
+
 });
 
 module.exports = router;
