c23330f4b3820b7fbd69a620b8f36c9cdb62cb34
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-06-29 11:24:18 +0200

    Update pool.query on login.js

--- routes/login.js
+++ routes/login.js
@@ -11,36 +11,57 @@ const pool = new Pool({
     port: 5432,
 });
 
+pool.connect();
+
+router.get('/', (req, res) => {
+    res.send('Please Use POST!');
+})
 
 router.post('/', (req, res) => {
     const { username, password } = req.body;
 
     pool
-        .query(`SELECT * FROM users WHERE username='${username}';`)
+        .query(`SELECT * FROM users WHERE username=$1;`, [ username ])
         .then((data) => {
             const returnedTable = data.rows; // Array of records
 
+            // If user doesn't exist in DB we response with error
             if (returnedTable.length === 0) {
                 // This return is to exit from function
                 return res.send({ error: 'Sorry no records found' });
             }
-
-            const userFromDb = returnedTable[0];
-
-            const detailedInfoAboutUser = {
-                firstName: userFromDb.first_name,
-                lastName: userFromDb.last_name,
-                secretToken: 'Secret!!!!!'
-            };
-
+            // And if it does:
+            // Checking password hashes
             // Building hash for received from request password
+            const userFromDb = returnedTable[0];
             const salt = 'SECRET_HERE';
             const receivedHashedPassword = md5(password + salt);
 
             const passwordsHashesMatch = userFromDb.password_hash === receivedHashedPassword;
 
+
             if (passwordsHashesMatch) {
-                res.send(detailedInfoAboutUser);
+                const tokenPayload = {
+                    firstName: userFromDb.first_name,
+                    lastName: userFromDb.last_name,
+                    username,
+                };
+
+                const secretForToken = 'SECRET_FOR_TOKEN';
+
+                const tokenPayloadAsString = JSON.stringify(tokenPayload); // Convert  payload to JSON string
+
+                // Convert string to base64 encoded string
+                const buff = new Buffer(tokenPayloadAsString); // Create new buffer object
+                const base64Payload = buff.toString('base64'); // Use buffer object to build first part of the token
+
+                // Build signature for payload
+                const signature = md5(base64Payload + secretForToken); // Second part of the token
+
+                // Combine payload and signature to one token
+                const detailedInfoAboutUser = `${base64Payload}.${signature}`;
+
+                res.send({ error: null, token: detailedInfoAboutUser });
             } else {
                 res.send({ error: 'Failed' });
             }
