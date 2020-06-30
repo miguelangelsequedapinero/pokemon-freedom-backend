0f44c3374e8cf3f71ef590a92cdec5e2ce7ce45e
gaiasamadhi <64321211+gaiasamadhi@users.noreply.github.com>
2020-06-30 18:10:29 +0200

    Update App (clean up)

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var pokemonRouter = require('./routes/pokemon');
-var loginRouter = require ('./routes/login');
-var newsRouter = require('./routes/news');
 
-var app = express();
+const pokemonRouter = require('./routes/pokemon');
+const loginRouter = require ('./routes/login');
+const newsRouter = require('./routes/news');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,8 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/pokemon', pokemonRouter);
 app.use('/login',loginRouter);
 app.use('/news', newsRouter);
